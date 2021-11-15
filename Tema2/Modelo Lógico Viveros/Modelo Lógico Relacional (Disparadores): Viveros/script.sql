9bafcaa8ee1033ba6c9f7635646fd88ef901e2ad
angelcapote <56321542+angelcapote@users.noreply.github.com>
2021-11-15 19:08:03 +0000

    Update script.sql

--- Tema2/Modelo Lógico Viveros/Modelo Lógico Relacional (Disparadores): Viveros/script.sql
+++ Tema2/Modelo Lógico Viveros/Modelo Lógico Relacional (Disparadores): Viveros/script.sql
@@ -148,8 +148,8 @@ CREATE OR REPLACE FUNCTION actualizar_stock() RETURNS TRIGGER AS $actualizar_sto
 	BEGIN
 		IF NEW.cantidad > 0 THEN
 			UPDATE PRODUCTOS
-			SET PRODUCTOS.stock = PRODUCTOS.stock-NEW.cantidad,
-			WHERE PRODUCTOS.idProductos = NEW.idProductos;
+			SET stock = stock-NEW.cantidad
+			WHERE idProductos = NEW.idProductos;
 		END IF;
 	END;
 $actualizar_stock$ LANGUAGE plpgsql;
